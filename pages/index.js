5b33969ad85bb12334351af919b6cd647ec64391
ChatBSV <ab@alanborger.com>
2023-05-20 09:17:36 +0200

    0.1.0

--- pages/index.js
+++ pages/index.js
@@ -43,26 +43,26 @@ const IndexPage = () => {
   const loadingAssistantMessage = {
     message: 'Loading.. Please wait...',
     isUser: false,
+    isFake: false,
     sender: 'Assistant',
   };
 
   const fakeAssistantMessage = {
     message: FAKE_ASSISTANT_MESSAGE,
     isUser: false,
+    isFake: true,
     sender: 'Assistant',
   };
 
   return (
     <div style={{ color: '#555', backgroundColor: '#f1f1f1', flexDirection: 'column', fontFamily: 'IBM Plex Sans, sans-serif', fontSize: '16px', fontWeight: 400, lineHeight: '22px', display: 'flex', position: 'fixed', top: 0, bottom: 0, left: 0, right: 0 }}>
       <Header />
-      <ChatBody chat={chat} fakeAssistantMessage={fakeAssistantMessage} isLoading={isLoading} isError={isError} />
+      <ChatBody chat={chat} fakeAssistantMessage={fakeAssistantMessage} />
 
-      {isLoading && (
-        <ChatBody chat={[loadingAssistantMessage]} isLoading={isLoading} isError={isError} />
-      )}
+      {isLoading && <ChatBody chat={[loadingAssistantMessage]} fakeAssistantMessage={fakeAssistantMessage} />}
 
       {isError && (
-        <ChatBody chat={[{ message: 'Ooops. Something went wrong. Please try again or come back later.', isUser: false, sender: 'Assistant' }]} isLoading={isLoading} isError={isError} />
+        <ChatBody chat={[{ message: 'Ooops. Something went wrong. Please try again or come back later.', isUser: false, isFake: false }]} fakeAssistantMessage={fakeAssistantMessage} />
       )}
 
       <div className="chat-footer">
@@ -73,3 +73,4 @@ const IndexPage = () => {
 };
 
 export default IndexPage;
+
