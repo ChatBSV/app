fa01758a80ff2c43c055ba24978f17d1ea485bb8
ChatBSV <ab@alanborger.com>
2023-05-20 05:04:13 +0200

    0.1.0

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import ChatBody from '../components/ChatBody';
 import ChatInput from '../components/ChatInput';
 import Header from '../components/Header';
+import './global.css';
+
 
 const IndexPage = () => {
   const [chat, setChat] = useState([]);
@@ -52,7 +54,9 @@ const IndexPage = () => {
     <div style={{ color: '#555', backgroundColor: '#f1f1f1', flexDirection: 'column', fontFamily: 'IBM Plex Sans, sans-serif', fontSize: '16px', fontWeight: 400, lineHeight: '22px', display: 'flex', position: 'fixed', top: 0, bottom: 0, left: 0, right: 0 }}>
       <Header />
       <ChatBody chat={chat} />
+      <div className="chat-footer">
       <ChatInput handleSubmit={handleSubmit} />
+    </div>
     </div>
   );
 };
