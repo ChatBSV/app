69063f2931ca4692a9bf46120d5a3ba5c125ba6f
ChatBSV <ab@alanborger.com>
2023-05-21 00:04:16 +0200

    Alfred 0.1.0

--- components/ChatMessage.js
+++ components/ChatMessage.js
@@ -5,7 +5,7 @@ import styles from './ChatMessage.module.css';
 function ChatMessage({ message, user, sender }) {
   return (
     <div className={`${styles.chatMessage} ${user ? styles.userMessage : styles.assistantMessage}`}>
-      <p className='paragraph'>{message}</p>
+      <span className='paragraph'>{message}</span>
       {sender && <span className={styles.sender}>{sender}</span>}
     </div>
   );
