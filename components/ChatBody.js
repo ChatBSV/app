996e71db77433c8eb668d28a8c0d6b58938138cb
ChatBSV <ab@alanborger.com>
2023-05-20 08:11:40 +0200

    0.1.0

--- components/ChatBody.js
+++ components/ChatBody.js
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react';
 import styles from './ChatBody.module.css';
 import ChatMessage from './ChatMessage';
 
-function ChatBody({ chat, fakeAssistantMessage }) {
+function ChatBody({ chat, fakeAssistantMessage, isLoading, isError }) {
   useEffect(() => {
     const chatContainer = document.getElementById('chat-container');
     chatContainer.scrollTop = chatContainer.scrollHeight;
@@ -23,6 +23,20 @@ function ChatBody({ chat, fakeAssistantMessage }) {
       {chat.length === 0 && fakeAssistantMessage && (
         <ChatMessage
           message={fakeAssistantMessage}
+          user={false}
+          sender="Assistant"
+        />
+      )}
+      {isLoading && (
+        <ChatMessage
+          message="Loading.. Please wait..."
+          user={false}
+          sender="Assistant"
+        />
+      )}
+      {isError && (
+        <ChatMessage
+          message="Ooops. Something went wrong. Please try again or come back later."
           user={false}
           sender="Assistant"
         />
--- pages/index.js
+++ pages/index.js
@@ -43,17 +43,12 @@ const IndexPage = () => {
   return (
     <div style={{ color: '#555', backgroundColor: '#f1f1f1', flexDirection: 'column', fontFamily: 'IBM Plex Sans, sans-serif', fontSize: '16px', fontWeight: 400, lineHeight: '22px', display: 'flex', position: 'fixed', top: 0, bottom: 0, left: 0, right: 0 }}>
       <Header />
-      <ChatBody chat={chat} fakeAssistantMessage={FAKE_ASSISTANT_MESSAGE} />
-      {isLoading && (
-        <div className="chatMessage assistantMessage loading">
-          Loading.. Please wait...
-        </div>
-      )}
-      {isError && (
-        <div className="chatMessage assistantMessage error">
-          Ooops. Something went wrong. Please try again or come back later.
-        </div>
-      )}
+      <ChatBody
+        chat={chat}
+        fakeAssistantMessage={FAKE_ASSISTANT_MESSAGE}
+        isLoading={isLoading}
+        isError={isError}
+      />
       <div className="chat-footer">
         <ChatInput handleSubmit={handleSubmit} />
       </div>
