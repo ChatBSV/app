ec02c713e5ac71f96525e8c23a90d052da6d9fd9
ChatBSV <ab@alanborger.com>
2023-05-20 04:55:03 +0200

    0.1.0

--- pages/index.js
+++ pages/index.js
@@ -21,30 +21,33 @@ const IndexPage = () => {
       },
     ];
   
+      setChat([...chat, { message: prompt, isUser: true }]);
+  
     try {
       const response = await axios.post(
-        '/.netlify/functions/getChatReply',
+        'https://api.openai.com/v1/chat/completions',
         {
-          body: prompt,
+          model: 'gpt-3.5-turbo',
+          messages: fullPrompt,
         },
         {
           headers: {
+            Authorization: `Bearer ${OPENAI_API_KEY}`,
             'Content-Type': 'application/json',
           },
         }
       );
   
-      const output = response.data.message;
+      const output = response.data.choices[0].message.content;
   
-      // Update the chat state with the new message
+     
       setChat([...chat, { message: output, isUser: false }]);
-  
     } catch (error) {
       console.error('Error:', error);
     }
   };
 
-  
+
   return (
     <div style={{ color: '#555', backgroundColor: '#f1f1f1', flexDirection: 'column', fontFamily: 'IBM Plex Sans, sans-serif', fontSize: '16px', fontWeight: 400, lineHeight: '22px', display: 'flex', position: 'fixed', top: 0, bottom: 0, left: 0, right: 0 }}>
       <Header />
