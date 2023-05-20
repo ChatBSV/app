6d08e57d0200b801fb13439d43753fc816f88e29
ChatBSV <ab@alanborger.com>
2023-05-20 10:22:06 +0200

    0.1.2

--- components/ChatBody.js
+++ components/ChatBody.js
@@ -1,33 +1,20 @@
 // components/ChatBody.js
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import styles from './ChatBody.module.css';
 import ChatMessage from './ChatMessage';
 
 function ChatBody({ chat, isLoading, isError }) {
-  const [hasIntroductionMessage, setHasIntroductionMessage] = useState(false);
-
   useEffect(() => {
     const chatContainer = document.getElementById('chat-container');
     chatContainer.scrollTop = chatContainer.scrollHeight;
   }, [chat]);
 
-  useEffect(() => {
-    if (chat.length > 0) {
-      const lastMessage = chat[chat.length - 1];
-      if (lastMessage.isUser) {
-        setHasIntroductionMessage(true);
-      }
-    }
-  }, [chat]);
-
   return (
     <div id="chat-container" className={styles.chatBody}>
-      {!hasIntroductionMessage && (
-        <div className={`${styles.chatMessage} ${styles.assistantMessage}`}>
-          <p>Hi there! I am Lillo.</p>
-        </div>
-      )}
+      <div className={`${styles.chatMessage} ${styles.assistantMessage}`}>
+        <p>Hi there! I am Lillo.</p>
+      </div>
 
       {chat.map((message, index) => (
         <ChatMessage
