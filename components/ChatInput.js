05790401d4a563403c2d54e6ad468a88fb843ac0
ChatBSV <ab@alanborger.com>
2023-05-20 06:13:40 +0200

    0.0.1

--- components/ChatInput.js
+++ components/ChatInput.js
@@ -35,3 +35,4 @@ const ChatInput = ({ handleSubmit }) => {
 };
 
 export default ChatInput;
+
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+// index.js
+
 import React, { useState } from 'react';
 import axios from 'axios';
 import ChatBody from '../components/ChatBody';
@@ -5,60 +7,35 @@ import ChatInput from '../components/ChatInput';
 import Header from '../components/Header';
 import './global.css';
 
-
 const IndexPage = () => {
   const [chat, setChat] = useState([]);
 
   const handleSubmit = async (prompt) => {
-    const { OPENAI_API_KEY, CORE_PROMPT } = process.env;
-  
-    const fullPrompt = [
-      {
-        role: 'system',
-        content: CORE_PROMPT,
-      },
-      {
-        role: 'user',
-        content: prompt,
-      },
-    ];
-  
-      setChat([...chat, { message: prompt, isUser: true }]);
-  
-    try {
-      const response = await axios.post(
-        'https://api.openai.com/v1/chat/completions',
-        {
-          model: 'gpt-3.5-turbo',
-          messages: fullPrompt,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${OPENAI_API_KEY}`,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-  
-      const output = response.data.choices[0].message.content;
-  
-     
-      setChat([...chat, { message: output, isUser: false }]);
-    } catch (error) {
-      console.error('Error:', error);
-    }
+    const response = await getChatReply(prompt);
+    const output = response.data.message;
+    setChat([...chat, { message: prompt, isUser: true }, { message: output, isUser: false }]);
   };
 
+  const getChatReply = async (prompt) => {
+    try {
+      const response = await axios.post('/.netlify/functions/getChatReply', { prompt });
+      return response;
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
+  };
 
   return (
     <div style={{ color: '#555', backgroundColor: '#f1f1f1', flexDirection: 'column', fontFamily: 'IBM Plex Sans, sans-serif', fontSize: '16px', fontWeight: 400, lineHeight: '22px', display: 'flex', position: 'fixed', top: 0, bottom: 0, left: 0, right: 0 }}>
       <Header />
       <ChatBody chat={chat} />
       <div className="chat-footer">
-      <ChatInput handleSubmit={handleSubmit} />
-    </div>
+        <ChatInput handleSubmit={handleSubmit} />
+      </div>
     </div>
   );
 };
 
 export default IndexPage;
+
