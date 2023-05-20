8ee49a87a226d1a92ed50ba5c74dfc8496955933
ChatBSV <ab@alanborger.com>
2023-05-21 00:14:38 +0200

    Alfred 0.1.0

--- components/ChatBody.js
+++ components/ChatBody.js
@@ -2,6 +2,7 @@
 
 import React, { useEffect } from 'react';
 import styles from './ChatBody.module.css';
+import styles from './ChatMessage.module.css';
 import ChatMessage from './ChatMessage';
 
 function ChatBody({ chat, isLoading, isError }) {
