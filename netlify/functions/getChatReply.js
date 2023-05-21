5b8cb3cb4ea4372b2334d4e0cbdf0aa9d9adb88d
ChatBSV <ab@alanborger.com>
2023-05-21 20:58:07 +0200

    4r10

--- netlify/functions/getChatReply.js
+++ netlify/functions/getChatReply.js
@@ -2,16 +2,17 @@
 
 const axios = require('axios');
 
+const axios = require('axios');
+
 exports.handler = async function (event, context) {
   const { OPENAI_API_KEY } = process.env;
   const { prompt, lastThreeMessages } = JSON.parse(event.body);
 
   const messages = [
     ...(lastThreeMessages ? lastThreeMessages.map(message => ({ role: 'user', content: message })) : []),
-    { role: 'system', content: process.env.CORE_PROMPT },
+    { role: 'system', content: CORE_PROMPT },
     { role: 'user', content: prompt }
   ];
-
 
   try {
     const response = await axios.post(
@@ -20,6 +21,7 @@ exports.handler = async function (event, context) {
         model: 'gpt-3.5-turbo',
         messages: messages,
         max_tokens: 2000,
+        log_level: 'info' // Include log level to get token counts
       },
       {
         headers: {
@@ -30,10 +32,12 @@ exports.handler = async function (event, context) {
     );
 
     const assistantResponse = response.data.choices[0].message.content;
-    const inputTokens = lastThreeMessages ? lastThreeMessages.reduce((count, message) => count + message.split(' ').length, 0) : 0;
+
+    // Calculate the total tokens consumed
+    const logs = response.data.choices[0].logprobs;
+    const inputTokens = logs.token_logprobs.reduce((count, tokenLog) => count + tokenLog.length, 0);
     const outputTokens = assistantResponse.split(' ').length;
     const totalTokens = inputTokens + outputTokens;
-
 
     return {
       statusCode: 200,
