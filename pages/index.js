fce9a6cbe032180a66ac82078b19871dc6310505
ChatBSV <ab@alanborger.com>
2023-05-21 10:03:06 +0200

    meta

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 // index.js
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import ChatBody from '../components/ChatBody';
 import ChatInput from '../components/ChatInput';
 import Header from '../components/Header';
 import Head from 'next/head';
-import './global.css';
+import '../styles/global.css';
 
 const IndexPage = () => {
   const [chat, setChat] = useState([]);
@@ -40,39 +40,21 @@ const IndexPage = () => {
     }
   };
 
-  useEffect(() => {
-    const addMetaTag = (name, content) => {
-      const metaElement = document.createElement('meta');
-      metaElement.name = name;
-      metaElement.content = content;
-      document.head.appendChild(metaElement);
-    };
-
-    const addLinkTag = (rel, href) => {
-      const linkElement = document.createElement('link');
-      linkElement.rel = rel;
-      linkElement.href = href;
-      document.head.appendChild(linkElement);
-    };
-
-    addMetaTag('description', 'Your local friendly interface to OpenAI. Ask me anything!');
-    addMetaTag('og:title', 'Hi there, I am AIfred.');
-    addMetaTag('og:description', 'Your local friendly interface to OpenAI. Ask me anything!');
-    addMetaTag('og:image', '/images/AL-og.png');
-    addMetaTag('twitter:title', 'Hi there, I am AIfred.');
-    addMetaTag('twitter:description', 'Your local friendly interface to OpenAI. Ask me anything!');
-    addMetaTag('twitter:image', '/images/AL-og.png');
-    addMetaTag('og:type', 'website');
-    addMetaTag('twitter:card', 'summary_large_image');
-
-    addLinkTag('icon', '/images/AL-favicon.png');
-    addLinkTag('apple-touch-icon', '/images/AL-webclip.png');
-  }, []);
-
   return (
     <div style={{ color: '#555', backgroundColor: '#f1f1f1', flexDirection: 'column', fontFamily: 'IBM Plex Sans, sans-serif', fontSize: '16px', fontWeight: 400, lineHeight: '22px', display: 'flex', position: 'fixed', top: 0, bottom: 0, left: 0, right: 0 }}>
       <Head>
         <title>Hi there, I am AIfred.</title>
+        <meta name="description" content="Your local friendly interface to OpenAI. Ask me anything!" />
+        <meta property="og:title" content="Hi there, I am AIfred." />
+        <meta property="og:description" content="Your local friendly interface to OpenAI. Ask me anything!" />
+        <meta property="og:image" content="/images/AL-og.png" />
+        <meta property="twitter:title" content="Hi there, I am AIfred." />
+        <meta property="twitter:description" content="Your local friendly interface to OpenAI. Ask me anything!" />
+        <meta property="twitter:image" content="/images/AL-og.png" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <link rel="icon" href="/images/AL-favicon.png" />
+        <link rel="apple-touch-icon" href="/images/AL-webclip.png" />
       </Head>
       <Header />
       <ChatBody chat={chat} isLoading={isLoading} isError={isError} />
@@ -83,4 +65,5 @@ const IndexPage = () => {
   );
 };
 
-export default IndexPage;
+export default
+
