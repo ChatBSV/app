7ad10ef80441e4bf8acb92351135d5c6241e1af0
ChatBSV <ab@alanborger.com>
2023-05-21 18:54:57 +0200

    4-f

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+// index.js
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ChatBody from '../components/ChatBody';
@@ -7,57 +9,46 @@ import Head from 'next/head';
 import './global.css';
 
 const IndexPage = () => {
+  const [chat, setChat] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const corePrompt = process.env.CORE_PROMPT || ''; // Fetch the core prompt from the environment
-  const [chat, setChat] = useState([{ message: corePrompt, isUser: false }]);
   const [sessionId, setSessionId] = useState('');
 
   useEffect(() => {
-    // Generate a new session ID
-    const newSessionId = generateSessionId();
-    setSessionId(newSessionId);
+    setSessionId(Date.now().toString());  // Creates a unique session ID based on the current timestamp
   }, []);
-
-  const generateSessionId = () => {
-    // Logic to generate a unique session ID
-    // Replace this with your own session ID generation logic
-    return Math.random().toString(36).substring(2, 10);
-  };
 
   const handleSubmit = async (prompt) => {
     setChat((prevChat) => [...prevChat, { message: prompt, isUser: true }]);
     setIsLoading(true);
     setIsError(false);
-  
-    try {
-      const corePrompt = chat[chat.length - 1].message; // Access corePrompt from the chat state
-      const response = await axios.post('/.netlify/functions/getChatReply', {
-        sessionId: sessionId,
-        corePrompt: corePrompt,
-        prompt
-      });
-  
-      setIsLoading(false);
-  
-      if (response.status === 200) {
-        const output = response.data.message;
-        setChat((prevChat) => [...prevChat, { message: output, isUser: false }]);
-      } else {
-        setIsError(true);
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      setIsLoading(false);
+
+    const response = await getChatReply(prompt, sessionId);
+
+    setIsLoading(false);
+
+    if (response) {
+      const output = response.data.message;
+      setChat((prevChat) => [...prevChat, { message: output, isUser: false }]);
+    } else {
       setIsError(true);
     }
   };
-  
+
+  const getChatReply = async (prompt, sessionId) => {
+    try {
+      const response = await axios.post('/.netlify/functions/getChatReply', { prompt, sessionId });
+      return response;
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
+  };
 
   return (
     <div style={{ color: '#555', backgroundColor: '#f1f1f1', flexDirection: 'column', fontFamily: 'IBM Plex Sans, sans-serif', fontSize: '16px', fontWeight: 400, lineHeight: '22px', display: 'flex', position: 'fixed', top: 0, bottom: 0, left: 0, right: 0 }}>
       <Head>
-        <meta name="description" content="Your local friendly interface to OpenAI. Ask me anything!" />
+      <meta name="description" content="Your local friendly interface to OpenAI. Ask me anything!" />
         <meta property="og:title" content="Hi there, I am AIfred." />
         <meta property="og:description" content="Your local friendly interface to OpenAI. Ask me anything!" />
         <meta property="og:image" content="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/6469d331b39363e2e343ad1a_AL-og.png" />
@@ -67,10 +58,7 @@ const IndexPage = () => {
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <link rel="icon" href="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/6469d331b39363e2e343ad07_AL-favicon.png" />
-        <link rel="apple-touch-icon" href="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/6469d331b39363e2e343ad07_AL-favicon.png" />
-        <meta property="og:title" content="Hi there, I am AIfred." />
-        <meta property="og:image" content="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/6469d331b39363e2e343ad1a_AL-og.png" />
-        <title>Hi there, I am AIfred.</title>
+        <link rel="apple-touch-icon" href="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/6469d33188cfb0d03f9067f7_AL-webclip.png" />
       </Head>
       <Header />
       <ChatBody chat={chat} isLoading={isLoading} isError={isError} />
