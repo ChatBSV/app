bcdd91302db769454c48437f1cddbef983125d76
ChatBSV <ab@alanborger.com>
2023-05-21 06:22:32 +0200

    auto-scroll

--- components/ChatBody.js
+++ components/ChatBody.js
@@ -1,49 +1,55 @@
 // components/ChatBody.js
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './ChatBody.module.css';
 import ChatMessage from './ChatMessage';
 
 function ChatBody({ chat, isLoading, isError }) {
+  const chatContainerRef = useRef(null);
+
   useEffect(() => {
-    const chatContainer = document.getElementById('chat-container');
-    chatContainer.scrollTop = chatContainer.scrollHeight;
+    scrollToBottom();
   }, [chat]);
+
+  const scrollToBottom = () => {
+    chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+  };
 
   return (
     <div className={styles.chatBody}>
-      <div className={styles.chatContainer} id="chat-container">
-      <ChatMessage
-        message="Welcome back Master Bruce. How can I help you today, sir?"
-        user={false}
-        className={styles.introMessage}
-      />
-
-      {chat.map((message, index) => (
+      <div className={styles.chatContainer} id="chat-container" ref={chatContainerRef}>
         <ChatMessage
-          key={index}
-          message={message.message}
-          user={message.isUser}
-        />
-      ))}
-
-      {isLoading && (
-        <ChatMessage
-          message="Loading.. Please wait..."
+          message="Welcome back Master Bruce. How can I help you today, sir?"
           user={false}
-          className={styles.loadingMessage}
+          className={styles.introMessage}
         />
-      )}
 
-      {isError && (
-        <ChatMessage
-          message="Ooops. Something went wrong. Please try again or come back later."
-          user={false}
-          className={styles.errorMessage}
-        />
-      )}
-    <div className={styles.spacer}></div>
-    </div></div>
+        {chat.map((message, index) => (
+          <ChatMessage
+            key={index}
+            message={message.message}
+            user={message.isUser}
+          />
+        ))}
+
+        {isLoading && (
+          <ChatMessage
+            message="Loading.. Please wait..."
+            user={false}
+            className={styles.loadingMessage}
+          />
+        )}
+
+        {isError && (
+          <ChatMessage
+            message="Ooops. Something went wrong. Please try again or come back later."
+            user={false}
+            className={styles.errorMessage}
+          />
+        )}
+        <div className={styles.spacer}></div>
+      </div>
+    </div>
   );
 }
 
