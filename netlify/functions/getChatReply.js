762ae5db8ee2d82b1af512eb048b2b1ac12eb1a1
ChatBSV <ab@alanborger.com>
2023-05-21 13:45:45 +0200

    context

--- netlify/functions/getChatReply.js
+++ netlify/functions/getChatReply.js
@@ -6,20 +6,18 @@ const NodeCache = require('node-cache');
 const conversationCache = new NodeCache();
 
 exports.handler = async function (event, context) {
-  const { OPENAI_API_KEY } = process.env;
-  const { corePrompt, prompt, memory, envs } = JSON.parse(event.body);
-
-  let fullPrompt = [];
+  const { OPENAI_API_KEY, CORE_PROMPT } = process.env;
+  const { prompt, memory, envs } = JSON.parse(event.body);
 
   const conversationHistory = memory || [];
 
-  if (conversationHistory.length > 1) {
-    fullPrompt.push(...conversationHistory.slice(0, -1)); // Exclude the last user message from the history
+  let fullPrompt = [];
+
+  if (conversationHistory.length > 0) {
+    fullPrompt.push({ role: 'system', content: CORE_PROMPT });
+    fullPrompt.push(...conversationHistory.slice(0, -1));
   }
 
-  fullPrompt.push({ role: 'system', content: corePrompt });
-  fullPrompt.push({ role: 'system', content: '' });
-  fullPrompt.push({ role: 'system', content: '' });
   fullPrompt.push({ role: 'user', content: prompt });
 
   try {
@@ -27,13 +25,7 @@ exports.handler = async function (event, context) {
       'https://api.openai.com/v1/chat/completions',
       {
         model: 'gpt-3.5-turbo',
-        messages: fullPrompt.map((message) => {
-          // Ensure each message object has the 'role' and 'content' properties
-          if (!message.role || !message.content) {
-            return { role: 'system', content: message.content || '' };
-          }
-          return message;
-        }),
+        messages: fullPrompt,
         max_tokens: 2000,
       },
       {
@@ -47,7 +39,7 @@ exports.handler = async function (event, context) {
 
     const output = response.data.choices[0].message.content;
 
-    conversationCache.set('history', conversationHistory.slice(-1)); // Save the last message in the conversation history
+    conversationCache.set('history', conversationHistory.slice(0, -1));
 
     return {
       statusCode: 200,
@@ -64,3 +56,4 @@ exports.handler = async function (event, context) {
     };
   }
 };
+
