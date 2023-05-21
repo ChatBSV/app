3474dd5cbeb4f2d4d0acdad2024e3734b9cd1516
ChatBSV <ab@alanborger.com>
2023-05-21 08:34:22 +0200

    metadata

--- pages/index.js
+++ pages/index.js
@@ -56,17 +56,18 @@ const IndexPage = () => {
   return (
     <div style={{ color: '#555', backgroundColor: '#f1f1f1', flexDirection: 'column', fontFamily: 'IBM Plex Sans, sans-serif', fontSize: '16px', fontWeight: 600, lineHeight: '22px', display: 'flex', position: 'fixed', top: 0, bottom: 0, left: 0, right: 0 }}>
       <NextHead>
-        <title>Hi there, I&apos;m Lillo.</title>
+        <title>Hi there, I'm Lillo.</title>
         <meta name="description" content="Your local friendly interface to OpenAI. Ask me anything!" />
-        <meta property="og:title" content="Hi there, I&apos;m Lillo." />
+        <meta property="og:title" content="Hi there, I'm Lillo." />
         <meta property="og:description" content="Your local friendly interface to OpenAI. Ask me anything!" />
         <meta property="og:image" content="../images/AL-og.png" />
-        <meta property="twitter:title" content="Hi there, I&apos;m Lillo." />
+        <meta property="twitter:title" content="Hi there, I'm Lillo." />
         <meta property="twitter:description" content="Your local friendly interface to OpenAI. Ask me anything!" />
         <meta property="twitter:image" content="../images/AL-og.png" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
       </NextHead>
+      {/* eslint-disable react/no-unescaped-entities */}
       <Header />
       <ChatBody chat={chat} isLoading={isLoading} isError={isError} />
       <div className="chat-footer">
@@ -77,4 +78,4 @@ const IndexPage = () => {
 };
 
 export default IndexPage;
-``
+
