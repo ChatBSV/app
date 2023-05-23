b60dfc42518911def4eb189a4ab906f319b65ac9
ChatBSV <ab@alanborger.com>
2023-05-24 00:09:24 +0200

    mb1

--- components/ChatInput.js
+++ components/ChatInput.js
@@ -1,10 +1,24 @@
 // components/ChatInput.js
 
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './ChatInput.module.css';
 
 const ChatInput = ({ handleSubmit }) => {
   const [input, setInput] = useState('');
+  const [moneyButtonLoaded, setMoneyButtonLoaded] = useState(false);
+  const moneyButtonRef = useRef(null);
+
+  useEffect(() => {
+    const moneyButtonScript = document.createElement('script');
+    moneyButtonScript.src = 'https://www.moneybutton.com/moneybutton.js';
+    moneyButtonScript.async = true;
+    moneyButtonScript.onload = () => setMoneyButtonLoaded(true);
+    document.body.appendChild(moneyButtonScript);
+
+    return () => {
+      document.body.removeChild(moneyButtonScript);
+    };
+  }, []);
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
@@ -18,6 +32,24 @@ const ChatInput = ({ handleSubmit }) => {
 
   const handleInputChange = (event) => setInput(event.target.value);
 
+  const handleMoneyButtonPayment = (payment) => {
+    const { txid } = payment;
+    console.log('Transaction ID:', txid);
+
+    // Fetch additional data or perform any necessary actions
+  };
+
+  useEffect(() => {
+    if (moneyButtonLoaded && moneyButtonRef.current) {
+      const moneyButton = window.moneyButton.render(moneyButtonRef.current, {
+        to: '3332',
+        amount: '0.0099',
+        currency: 'USD',
+        onPayment: handleMoneyButtonPayment
+      });
+    }
+  }, [moneyButtonLoaded]);
+
   return (
     <div className={styles.chatFooter}>
       <form onSubmit={handleFormSubmit} className={styles.inputForm}>
@@ -28,7 +60,7 @@ const ChatInput = ({ handleSubmit }) => {
           className={styles.inputField}
           placeholder="Enter your prompt..."
         />
-        <button type="submit" className={styles.submit}></button>
+        <div ref={moneyButtonRef} className={styles.moneyButton}></div>
       </form>
     </div>
   );
