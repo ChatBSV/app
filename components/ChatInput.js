295dc1b30da5d49a4ecec8e155ec808aef5f8b37
ChatBSV <ab@alanborger.com>
2023-05-23 09:06:37 +0200

    BSV

--- components/ChatInput.js
+++ components/ChatInput.js
@@ -2,34 +2,47 @@
 
 import React, { useState } from 'react';
 import styles from './ChatInput.module.css';
+import MoneyButton from '@moneybutton/react-money-button';
+import '@moneybutton/react-money-button/dist/moneybutton.css';
+
 
 const ChatInput = ({ handleSubmit }) => {
   const [input, setInput] = useState('');
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    const prompt = input.trim();
+    // Submission now managed by MoneyButton onPayment
+  };
 
+  const handleInputChange = (event) => setInput(event.target.value);
+
+  const handlePayment = (payment) => {
+    const prompt = input.trim();
     if (prompt !== '') {
       handleSubmit(prompt);
       setInput('');
     }
   };
 
-  const handleInputChange = (event) => setInput(event.target.value);
-
   return (
     <div className={styles.chatFooter}>
-      <form onSubmit={handleFormSubmit} className={styles.inputForm}>
-        <input
-          type="text"
-          value={input}
-          onChange={handleInputChange}
-          className={styles.inputField}
-          placeholder="Enter your prompt"
-        />
-        <button type="submit" className={styles.submit}></button>
-      </form>
+      <div className="moneyButton">
+        <form onSubmit={handleFormSubmit} className={styles.inputForm}>
+          <input
+            type="text"
+            value={input}
+            onChange={handleInputChange}
+            className={styles.inputField}
+            placeholder="Enter your prompt"
+          />
+          <MoneyButton
+            to="3332"
+            amount="0.0099"
+            currency="USD"
+            onPayment={handlePayment}
+          />
+        </form>
+      </div>
     </div>
   );
 };
