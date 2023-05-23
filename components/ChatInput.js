15a9bf23b78377bdd6589dff46e0638a03cf32ba
ChatBSV <ab@alanborger.com>
2023-05-23 22:15:30 +0200

    mb

--- components/ChatInput.js
+++ components/ChatInput.js
@@ -1,12 +1,10 @@
 // components/ChatInput.js
 
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import styles from './ChatInput.module.css';
 import axios from 'axios';
 
 const ChatInput = ({ handleSubmit }) => {
-  const [input, setInput] = useState('');
-
   const fetchTxid = async () => {
     try {
       const response = await axios.get('https://api.moneybutton.com/v2/auth/bsvalias/resolve', {
@@ -32,38 +30,34 @@ const ChatInput = ({ handleSubmit }) => {
     window.moneyButtonRender = async () => {
       const prompt = input.trim();
       if (prompt !== '') {
-        setInput('');
         handleSubmit(prompt);
       }
     };
 
-    window.moneyButtonOnPayment = async (payment) => {
+    const renderMoneyButton = async () => {
       const txid = await fetchTxid();
-      setInput(payment.buttonData);
-      handleSubmit(payment.buttonData, txid);
+      const div = document.createElement('div');
+      div.id = 'money-button';
+      document.getElementById('button-container').appendChild(div);
+
+      const moneyButtonOptions = {
+        to: '3332',
+        amount: '0.0099',
+        currency: 'USD',
+        onPayment: 'moneyButtonRender', // Call the moneyButtonRender function
+      };
+
+      window.moneyButton.render(div, moneyButtonOptions);
     };
 
-    // Render MoneyButton on load
-    const div = document.createElement('div');
-    div.id = 'money-button';
-    document.getElementById('button-container').appendChild(div);
-
-    const moneyButtonOptions = {
-      to: '3332',
-      amount: '0.0099',
-      currency: 'USD',
-      onPayment: 'moneyButtonOnPayment', // Call the onPayment callback function
-    };
-
-    window.moneyButton.render(div, moneyButtonOptions);
-  }, [handleSubmit, input]);
+    renderMoneyButton();
+  }, [handleSubmit]);
 
   return (
     <div className={styles.chatFooter}>
       <form className={styles.inputForm}>
         <input
           type="text"
-          value={input}
           readOnly
           className={styles.inputField}
           placeholder="Enter your prompt"
