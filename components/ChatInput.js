e53f7b252d76b9dd5fe4393fa5995eed03c170c4
ChatBSV <ab@alanborger.com>
2023-05-23 22:27:36 +0200

    nb

--- components/ChatInput.js
+++ components/ChatInput.js
@@ -1,69 +1,61 @@
-// components/ChatInput.js
-
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './ChatInput.module.css';
-import axios from 'axios';
 
 const ChatInput = ({ handleSubmit }) => {
-  const fetchTxid = async () => {
-    try {
-      const response = await axios.get('https://api.moneybutton.com/v2/auth/bsvalias/resolve', {
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-      });
+  const [input, setInput] = useState('');
 
-      return response.data.txid;
-    } catch (error) {
-      console.error('Error fetching txid:', error);
-      return null;
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const prompt = input.trim();
+
+    if (prompt !== '') {
+      handleSubmit(prompt);
+      setInput('');
     }
   };
 
-  useEffect(() => {
-    const moneyButtonScript = document.createElement('script');
-    moneyButtonScript.src = 'https://www.moneybutton.com/moneybutton.js';
-    moneyButtonScript.async = true;
-    document.body.appendChild(moneyButtonScript);
+  const handleInputChange = (event) => setInput(event.target.value);
 
-    window.moneyButtonRender = async () => {
-      const prompt = input.trim();
-      if (prompt !== '') {
-        handleSubmit(prompt);
-      }
-    };
-
-    const renderMoneyButton = async () => {
-      const txid = await fetchTxid();
-      const div = document.createElement('div');
-      div.id = 'money-button';
-      document.getElementById('button-container').appendChild(div);
-
-      const moneyButtonOptions = {
-        to: '3332',
-        amount: '0.0099',
-        currency: 'USD',
-        onPayment: 'moneyButtonRender', // Call the moneyButtonRender function
-      };
-
-      window.moneyButton.render(div, moneyButtonOptions);
-    };
-
-    renderMoneyButton();
-  }, [handleSubmit]);
+  const handlePayment = async (payment) => {
+    const prompt = input.trim();
+    if (prompt !== '') {
+      handleSubmit(prompt);
+      setInput('');
+    }
+  };
 
   return (
     <div className={styles.chatFooter}>
-      <form className={styles.inputForm}>
+      <form onSubmit={handleFormSubmit} className={styles.inputForm}>
         <input
           type="text"
-          readOnly
+          value={input}
+          onChange={handleInputChange}
           className={styles.inputField}
           placeholder="Enter your prompt"
         />
-        <div id="button-container"></div>
+        <div id="money-button-container"></div>
+        <button type="submit" className={styles.submit}></button>
       </form>
+
+      <script
+        src="https://www.moneybutton.com/moneybutton.js"
+        async
+        onLoad={() => {
+          const moneyButtonContainer = document.getElementById('money-button-container');
+          window.moneyButton.render(moneyButtonContainer, {
+            to: '3332',
+            amount: '0.01',
+            currency: 'USD',
+            label: '',
+            clientIdentifier: 'ac20c624759297bcdd5b0db272d839d9',
+            buttonId: '1684033604257',
+            buttonData: '{}',
+            type: 'tip',
+            onPayment: handlePayment
+          });
+        }}
+      ></script>
     </div>
   );
 };
