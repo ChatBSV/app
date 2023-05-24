e8d2bf113ee41ee10192867471b39cdc53c170b0
ChatBSV <ab@alanborger.com>
2023-05-24 03:34:44 +0200

    mb

--- components/ChatMessage.js
+++ components/ChatMessage.js
@@ -3,27 +3,21 @@
 import React from 'react';
 import styles from './ChatMessage.module.css';
 
-function ChatMessage({ message, user, tokens, txid }) {
-  const isAssistantMessage = !user;
-  const isUserAssistant = user && isAssistantMessage;
+function ChatMessage({ message, role, tokens, txid }) {
+  const isUserMessage = role === 'user';
+  const isAssistantMessage = role === 'assistant';
 
   return (
-    <div className={`${styles.chatMessage} ${isUserAssistant ? styles.assistantMessage : ''}`}>
-      {isAssistantMessage ? (
-        <div>
-          <span className={styles.message}>{message}</span>
-          {txid && (
-            <div className={styles.link}>
-              <a href={`https://whatsonchain.com/tx/${txid}`} target="_blank" rel="noopener noreferrer" className={styles.linkIcon}>
-                <img src="x_link-icon" alt="Link Icon" />
-              </a>
-              <span className={styles.txid}>{txid}</span>
-              <span className={styles.tokens}>{tokens} Tokens</span>
-            </div>
-          )}
+    <div className={`${styles.chatMessage} ${isUserMessage ? styles.userMessage : ''} ${isAssistantMessage ? styles.assistantMessage : ''}`}>
+      <span className={styles.message}>{message}</span>
+      {isAssistantMessage && txid && (
+        <div className={styles.link}>
+          <a href={`https://whatsonchain.com/tx/${txid}`} target="_blank" rel="noopener noreferrer" className={styles.linkIcon}>
+            <img src="x_link-icon" alt="Link Icon" />
+          </a>
+          <span className={styles.txid}>{txid}</span>
+          <span className={styles.tokens}>{tokens} Tokens</span>
         </div>
-      ) : (
-        <span className={styles.message}>{message}</span>
       )}
     </div>
   );
