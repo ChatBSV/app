bd0cfd9ce66f4843b283821070f434a667e049b8
ChatBSV <ab@alanborger.com>
2023-05-24 04:35:03 +0200

    mb

--- components/ChatMessage.js
+++ components/ChatMessage.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import styles from './ChatMessage.module.css';
+import Image from 'next/image';
 
 function ChatMessage({ message, role, totalTokens, txid }) {
   const isUserMessage = role === 'user';
@@ -11,10 +12,10 @@ function ChatMessage({ message, role, totalTokens, txid }) {
     <div className={`${styles.chatMessage} ${isUserMessage ? styles.userMessage : ''} ${isAssistantMessage ? styles.assistantMessage : ''}`}>
       <span className={styles.message} style={{ fontSize: '16pt', margin: 0 }}>{message}</span>
       {isAssistantMessage && (
-        <div className={styles.link}>
-          <a href={`https://whatsonchain.com/tx/${txid}`} target="_blank" rel="noopener noreferrer" className={styles.linkIcon}>
-            <img src="/link-icon.png" alt="Link Icon" />
-          </a>
+  <div className={styles.link}>
+    <a href={`https://whatsonchain.com/tx/${txid}`} target="_blank" rel="noopener noreferrer" className={styles.linkIcon}>
+      <Image src="/link-icon.png" alt="Link Icon" width={20} height={20} />
+    </a>
           <span className={styles.txid}>{txid}</span>
           <span className={styles.tokens}>{totalTokens} Tokens</span>
         </div>
