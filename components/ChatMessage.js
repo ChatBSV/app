9df090296aac48edcb00edf3640aea5e829990cd
ChatBSV <ab@alanborger.com>
2023-05-24 03:25:39 +0200

    mb hide chat link

--- components/ChatMessage.js
+++ components/ChatMessage.js
@@ -5,19 +5,22 @@ import styles from './ChatMessage.module.css';
 
 function ChatMessage({ message, user, tokens, txid }) {
   const isAssistantMessage = !user;
+  const isAssistantMessageReal = isAssistantMessage && txid;
 
   return (
-    <div className={`${styles.chatMessage} ${user ? styles.userMessage : styles.assistantMessage}`}>
+    <div className={`${styles.chatMessage} ${user ? styles.userMessage : ''} ${isAssistantMessageReal ? styles.assistantMessageReal : ''}`}>
       {isAssistantMessage ? (
         <div>
           <span className={styles.message}>{message}</span>
-          <div className={styles.link}>
-            <a href={`https://whatsonchain.com/tx/${txid}`} target="_blank" rel="noopener noreferrer" className={styles.linkIcon}>
-              <img src="x_link-icon" alt="Link Icon" />
-            </a>
-            <span className={styles.txid}>{txid}</span>
-            <span className={styles.tokens}>{tokens} Tokens</span>
-          </div>
+          {isAssistantMessageReal && (
+            <div className={styles.link}>
+              <a href={`https://whatsonchain.com/tx/${txid}`} target="_blank" rel="noopener noreferrer" className={styles.linkIcon}>
+                <img src="x_link-icon" alt="Link Icon" />
+              </a>
+              <span className={styles.txid}>{txid}</span>
+              <span className={styles.tokens}>{tokens} Tokens</span>
+            </div>
+          )}
         </div>
       ) : (
         <span className={styles.message}>{message}</span>
