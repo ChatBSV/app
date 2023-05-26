a89d441ed5d86226d8be06b7fbf0c58898612c19
ChatBSV <ab@alanborger.com>
2023-05-26 19:17:52 +0200

    log

--- netlify/functions/getChatReply.js
+++ netlify/functions/getChatReply.js
@@ -40,6 +40,8 @@ exports.handler = async function (event, context) {
     const assistantResponse = response.data.choices[0].message.content;
     const tokens = response.data.choices[0].message.total_tokens;
 
+    console.log('Tokens:', tokens); // Log the tokens value
+
     return {
       statusCode: 200,
       body: JSON.stringify({ message: assistantResponse, tokens, txid }),
