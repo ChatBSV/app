fa82eeb96e7121a615c27d38519ad273033443e1
ChatBSV <ab@alanborger.com>
2023-05-29 09:32:01 +0200

    revert

--- netlify/functions/getChatReply.js
+++ netlify/functions/getChatReply.js
@@ -14,7 +14,10 @@ exports.handler = async function (event, context) {
     );
 
     messages = [
-      { role: 'assistant', content: lastAssistantMessage.content },
+      ...history.map((message) => ({
+        role: message.role,
+        content: message.content,
+      })),
       { role: 'user', content: prompt },
     ];
   } else {
@@ -29,7 +32,7 @@ exports.handler = async function (event, context) {
       'https://api.openai.com/v1/chat/completions',
       {
         model: 'gpt-3.5-turbo',
-        messages: messages.map(message => ({ role: message.role, content: message.content })),
+        messages: messages,
         max_tokens: 2000,
       },
       {
