3b831c8979fcb6c9eea90ea1725988906336573d
ChatBSV <ab@alanborger.com>
2023-05-30 19:43:13 +0200

    0.1.0

--- components/ChatBody.js
+++ components/ChatBody.js
@@ -26,15 +26,13 @@ function ChatBody({ chat, isLoading, isError }) {
           className={styles.introMessage}
         />
   
-        {chat.map((message) => (
-          <ChatMessage
-            key={message.id}
-            content={message.content} // Update key to 'content'
-            role={message.role}
-            tokens={message.role === 'assistant' ? message.tokens : 0}
-            txid={message.txid}
-          />
-        ))}
+  {chat.map((message) => (
+  <ChatMessage
+    key={message.id}
+    {...message}
+  />
+))}
+
   
         {isLoading && (
           <ChatMessage
