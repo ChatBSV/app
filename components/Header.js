9e740857e2a8c2de2847275293c1ea2c4d96ab18
ChatBSV <ab@alanborger.com>
2023-09-04 13:08:26 +0200

    css

--- components/Header.js
+++ components/Header.js
@@ -30,7 +30,7 @@ function Header({ resetChat, redirectionUrl, sessionToken, user }) {
                 </div> */}
                 <ButtonIcon icon={user.avatarUrl} text={user.handle} />
                 <button
-                    className="rounded-lg px-3 py-1.5 text-xs bg-white/20 shadow-sm shadow-white/20 invisible group-hover:visible font-semibold text-red-400 hover:text-red-500 border border-transparent hover:border-red-500/30"
+                    className="rounded-lg px-3 py-1.5 text-xs bg-white/20 shadow-sm shadow-white/20 invisible group-hover:visible font-semibold text-red-400 hover:text-red-500 border border-transparent "
                     onClick={onDisconnect}>Disconnect
                 </button>
             </div>
@@ -42,13 +42,13 @@ function Header({ resetChat, redirectionUrl, sessionToken, user }) {
       </div>
       :        <div className={`w-full mb-4 flex justify-between items-end ${styles.loginButton}`}>
         <a href={redirectionUrl}>
-        <ButtonIcon icon="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/64f5b1e66dcd597fb1af816d_648029610832005036e0f702_hc%201.svg" text="connect" />
+        <ButtonIcon icon="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/64f5b1e66dcd597fb1af816d_648029610832005036e0f702_hc%201.svg" text="Connect" />
         {/* <div className="bg-darkBackground-700 rounded-full border m-0 hover:bg-darkBackground-900">
                     <div className="flex gap-x-3 pr-6">
                         <img src="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/64f5b1e66dcd597fb1af816d_648029610832005036e0f702_hc%201.svg"
                               className="inline-block w-8 h-8 border-black/50 rounded-full border-r border-t border-b opacity-90 bg-darkBackground-900"/>
                         <div className="flex flex-col justify-center items-start gap-y-0.5">
-                            <span className="font-bold text-white/90 leading-4">connect</span>
+                            <span className="font-bold text-white/90 leading-4">Connect</span>
                         </div>
                     </div>
                 </div> */}
