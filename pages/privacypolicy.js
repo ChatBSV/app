566cbf47f23341513b08b56113b76d726273c0e9
ChatBSV <ab@alanborger.com>
2023-09-06 01:33:23 +0200

    pp

--- pages/privacypolicy.js
+++ pages/privacypolicy.js
@@ -18,11 +18,10 @@ const PrivacyPolicy = () => {
       <p>Information You Share with Us</p>
       <p>Public Username. We collect your public username from your HandCash wallet when you connect it to our service.</p>
       <p>Information Automatically Collected</p>
-      <p>Usage Data. We collect information about your activity on our website, which is stored locally on your browser.</p>
+      <p>We do not collect information about your activity on our website, it is stored locally on your browser and we have no access to it.</p>
       
       <h2>How Do We Use Your Information?</h2>
       <p>To Facilitate Micro Payments. We use your public username to trigger micro payments via your HandCash wallet.</p>
-      <p>To Enhance User Experience. We use locally stored data to improve your experience while using our service.</p>
       
       <h2>Will Your Information be Shared with Anyone?</h2>
       <p>We do not share or store your information on our servers. All data is stored locally on your browser and is used to facilitate micro payments through your HandCash wallet.</p>
