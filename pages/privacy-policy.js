65bf66e8863649e3ae09df582a3a58a9587cf35d
ChatBSV <ab@alanborger.com>
2023-09-06 01:03:50 +0200

    pp

---
+++ pages/privacy-policy.js
@@ -0,0 +1,52 @@
+// pages/PrivacyPolicy.js
+
+import Head from 'next/head';
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <Head>
+        <title>Privacy Policy - ChatBSV</title>
+      </Head>
+      Privacy Policy for ChatBSV
+
+
+Last Updated: Sep 06 2023
+
+Introduction
+
+Welcome to ChatBSV. We are committed to protecting your personal information and your right to privacy. If you have any questions or concerns about our policy or our practices with regards to your personal information, please contact us at [Contact Email].
+
+What Information Do We Collect?
+Information You Share with Us
+Public Username. We collect your public username from your HandCash wallet when you connect it to our service.
+Information Automatically Collected
+Usage Data. We collect information about your activity on our website, which is stored locally on your browser.
+How Do We Use Your Information?
+To Facilitate Micro Payments. We use your public username to trigger micro payments via your HandCash wallet.
+To Enhance User Experience. We use locally stored data to improve your experience while using our service.
+Will Your Information be Shared with Anyone?
+We do not share or store your information on our servers. All data is stored locally on your browser and is used to facilitate micro payments through your HandCash wallet.
+
+How Long Do We Keep Your Information?
+We do not store your information on our servers. All data is stored locally on your browser and is deleted when you disconnect your HandCash wallet or clear your browser data.
+
+How Do We Keep Your Information Safe?
+All data is stored locally on your browser, and we do not have access to it. We do not store any data on our servers.
+
+Do We Collect Information from Minors?
+We do not knowingly collect data from or market to children under 18 years of age.
+
+Your Privacy Rights
+Since we do not collect or store your data on our servers, we do not have a process for you to request access or deletion of your data. You can manage all your information locally on your browser.
+
+Updates to this Policy
+We may update this privacy policy from time to time. The updated version will be indicated by an updated "Revised" date and the updated version will be effective as soon as it is accessible.
+
+Contact Us
+If you have questions or comments about this policy, you may contact us at hello@chatbsv.com.
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
