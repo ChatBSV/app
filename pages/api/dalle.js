5b26de46b1cc745b205eaa8c31e7bbeba24cf77f
ChatBSV <ab@alanborger.com>
2023-10-29 01:17:49 +0200

    dalle

--- pages/api/dalle.js
+++ pages/api/dalle.js
@@ -37,15 +37,16 @@ async function handleDalleRequest(reqBody) {
 
 // Default export to handle POST requests
 export default async (req, res) => {
+  console.log("Received request with method:", req.method); // Added logging
+  console.log("Received request with body:", req.body); // Added logging
+
   if (req.method !== "POST") {
     res.status(405).end(); // Method Not Allowed
     return;
   }
 
-  const reqBody = JSON.parse(req.body);
-
   try {
-    const result = await handleDalleRequest(reqBody);
+    const result = await handleDalleRequest(req.body); // Removed JSON.parse
     res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ error: error.message });
