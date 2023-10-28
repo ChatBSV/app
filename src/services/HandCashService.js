1ee4c3820265f5423aa891448f3f9a61e3f92bd0
ChatBSV <ab@alanborger.com>
2023-10-28 22:50:47 +0200

    imagine

--- src/services/HandCashService.js
+++ src/services/HandCashService.js
@@ -1,32 +1,36 @@
-import {Environments, HandCashConnect} from "@handcash/handcash-connect";
+// src/services/HandCashService.js
+import { Environments, HandCashConnect } from "@handcash/handcash-connect";
 
-const appId = process.env.handcash_app_id;
-const appSecret = process.env.handcash_app_secret;
+const appId = process.env.HANDCASH_APP_ID;  // Updated to match Vercel dashboard
+const appSecret = process.env.HANDCASH_APP_SECRET;  // Updated to match Vercel dashboard
+
+console.log("HandCashService: appId:", appId);  // Debug log
+console.log("HandCashService: appSecret:", appSecret);  // Debug log
 
 const handCashConnect = new HandCashConnect({
-    appId: appId,
-    appSecret: appSecret,
+  appId: appId,
+  appSecret: appSecret,
 });
 
 export default class HandCashService {
-    constructor(authToken) {
-        this.account = handCashConnect.getAccountFromAuthToken(authToken);
-    }
+  constructor(authToken) {
+    this.account = handCashConnect.getAccountFromAuthToken(authToken);
+  }
 
-    async getProfile() {
-        return this.account.profile.getCurrentProfile();
-    }
+  async getProfile() {
+    return this.account.profile.getCurrentProfile();
+  }
 
-    async pay({destination, amount, currencyCode, description}) {
-        return this.account.wallet.pay({
-            payments: [
-                {destination, amount, currencyCode},
-            ],
-            description: description || 'Sent from the HandCash',
-        });
-    }
+  async pay({ destination, amount, currencyCode, description }) {
+    return this.account.wallet.pay({
+      payments: [
+        { destination, amount, currencyCode },
+      ],
+      description: description || 'Sent from the HandCash',
+    });
+  }
 
-    getRedirectionUrl() {
-        return handCashConnect.getRedirectionUrl();
-    }
+  getRedirectionUrl() {
+    return handCashConnect.getRedirectionUrl();
+  }
 }
