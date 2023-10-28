a3800e1ed7baf29e79cf557e58520f69108f02aa
ChatBSV <ab@alanborger.com>
2023-10-29 01:25:36 +0200

    dalle log

--- pages/api/dalle.js
+++ pages/api/dalle.js
@@ -7,14 +7,18 @@ dotenv.config();
 // Function to handle DALLE request
 async function handleDalleRequest(reqBody) {
   const { prompt, format } = reqBody;
+  console.log('Entered handleDalleRequest with:', { prompt, format });
 
   if (!prompt || !format) {
+    console.error("Prompt and format are required");
     throw new Error("Prompt and format are required");
   }
 
   const apiEndpoint = "https://api.openai.com/v1/images/generations";
+  console.log('Using API endpoint:', apiEndpoint);
 
   try {
+    console.log('About to make POST request to DALLE API.');
     const response = await axios.post(apiEndpoint, {
       prompt,
       n: 1,
@@ -26,8 +30,10 @@ async function handleDalleRequest(reqBody) {
         'Content-Type': 'application/json'
       }
     });
+    console.log('Received response from DALLE API:', response);
 
     const imageUrl = response.data.url;
+    console.log('Generated image URL:', imageUrl);
     return { imageUrl };
   } catch (error) {
     console.error("Error generating image:", error);
@@ -37,18 +43,22 @@ async function handleDalleRequest(reqBody) {
 
 // Default export to handle POST requests
 export default async (req, res) => {
-  console.log("Received request with method:", req.method); // Added logging
-  console.log("Received request with body:", req.body); // Added logging
+  console.log("Received request with method:", req.method);
+  console.log("Received request with body:", req.body);
 
   if (req.method !== "POST") {
+    console.log('Method not allowed. Only POST is supported.');
     res.status(405).end(); // Method Not Allowed
     return;
   }
 
   try {
-    const result = await handleDalleRequest(req.body); // Removed JSON.parse
+    console.log('Attempting to handle DALLE request.');
+    const result = await handleDalleRequest(req.body);
+    console.log('Successfully handled DALLE request. Returning:', result);
     res.status(200).json(result);
   } catch (error) {
+    console.log('Failed to handle DALLE request. Error:', error.message);
     res.status(500).json({ error: error.message });
   }
 };
