409a7bba04fce9433b825ab63e211fd6d57bae21
ChatBSV <ab@alanborger.com>
2023-10-29 06:20:51 +0100

    n/a

--- components/ChatMessage.js
+++ components/ChatMessage.js
@@ -41,30 +41,34 @@ function ChatMessage({ content, role, tokens, txid }) {
       )}
       {shouldShowWidget && !isLoadingMessage && (
         <div className={styles.chatLink}>
-          <a
-            className={`${styles.copyButton} copyButton`}
-            href={`https://whatsonchain.com/tx/${txid}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{
-              textDecoration: 'none',
-            }}
-          >
-            <img
-              className={styles.copyIcon}
-              src="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/6474a9bf4a0547694b83498c_linked.svg"
-              alt="Transaction Link"
-            />
-            <span 
+          {txid ? (  // Add a check here to ensure txid is not null
+            <a
+              className={`${styles.copyButton} copyButton`}
+              href={`https://whatsonchain.com/tx/${txid}`}
+              target="_blank"
+              rel="noopener noreferrer"
               style={{
-                fontSize: '12px',
-                color: 'gray',
                 textDecoration: 'none',
               }}
             >
-              TxID:{txid.slice(0, 5)}
-            </span>
-          </a>
+              <img
+                className={styles.copyIcon}
+                src="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/6474a9bf4a0547694b83498c_linked.svg"
+                alt="Transaction Link"
+              />
+              <span 
+                style={{
+                  fontSize: '12px',
+                  color: 'gray',
+                  textDecoration: 'none',
+                }}
+              >
+                TxID:{txid.slice(0, 5)}
+              </span>
+            </a>
+          ) : (
+            <span>No transaction ID available.</span>
+          )}
           <img
             className={styles.copyIcon}
             src="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/6474a81e31e9c343912ede78_coins.svg"
