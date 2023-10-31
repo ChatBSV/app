26b7da458638436b62a9e669296e69cced84c6df
ChatBSV <ab@alanborger.com>
2023-10-31 09:53:38 +0100

    pay

--- pages/api/pay.js
+++ pages/api/pay.js
@@ -4,34 +4,41 @@ import HandCashService from "../../src/services/HandCashService";
 import SessionTokenRepository from "../../src/repositories/SessionTokenRepository";
 
 export default async function handler(req, res) {
+    console.log('pay.js: Entered handler');
     if (req.method !== 'POST') {
         return res.status(404).json({error: 'Not a POST request'});
     }
     try {
         const {authorization, requestType} = req.headers;  // Extract requestType from headers
+        console.log('pay.js: Authorization header:', authorization);
+        console.log('pay.js: Request type:', requestType);  // Log the request type
+        
         const sessionToken = authorization.split(' ')[1];
         if (!sessionToken) {
             return res.status(401).json({error: 'Missing authorization.'});
         }
 
-        const authTokenRepo = new AuthTokenRepository();
-        const sessionTokenRepo = new SessionTokenRepository();
-        const isSessionTokenValid = sessionTokenRepo.verify(sessionToken);
-
-        if (!isSessionTokenValid) {
-          return res.status(401).json({ error: 'Invalid session token' });
+        const {sessionId, user} = SessionTokenRepository.verify(sessionToken);
+        const authToken = AuthTokenRepository.getById(sessionId);
+        if (!authToken) {
+            return res.status(401).json({status: 'error', error: 'Expired authorization.'});
         }
 
         // Determine the payment amount based on the request type
-        let paymentAmount = parseFloat(process.env.CHAT_AMOUNT);  // Default amount for chat
+        let paymentAmount = process.env.CHAT_AMOUNT;  // Default amount for chat
         if (requestType === 'image') {
-            paymentAmount = parseFloat(process.env.IMAGE_AMOUNT);  // Amount for DALL-E image request
+            paymentAmount = process.env.IMAGE_AMOUNT;  // Amount for DALL-E image request
         }
+        console.log('pay.js: Using paymentAmount:', paymentAmount);  // Debug log to check used paymentAmount
 
-        const handCashService = new HandCashService();
-        const paymentResult = await handCashService.pay(paymentAmount, requestType, sessionToken);
 
-        res.status(200).json(paymentResult);
+        const paymentResult = await new HandCashService(authToken).pay({
+            destination: process.env.DEST, 
+            amount: paymentAmount,  // Use the determined amount
+            currencyCode: process.env.CURRENCY, 
+            description: 'ChatBSV payment'
+        });
+        return res.status(200).json({status: 'sent', transactionId: paymentResult.transactionId});
     } catch (error) {
         console.error('pay.js: Error', error);
         return res.status(400).json({status: 'error', message: error.toString()});
