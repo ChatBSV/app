9cdc694a9a0f7e56b4832e7b8f78ebfed201a056
ChatBSV <ab@alanborger.com>
2023-11-02 00:15:25 +0100

    1.1.5

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Cormorant } from 'next/font/google'
+import { Inter } from 'next/font/google'
 
-const Cormorant = Cormorant({ subsets: ['latin'] })
+const Inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={Cormorant.className}>{children}</body>
+      <body className={Inter.className}>{children}</body>
     </html>
   )
 }
