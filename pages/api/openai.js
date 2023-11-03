57d545603c56347e5e37dbf801de478c12f43b08
ChatBSV <ab@alanborger.com>
2023-11-03 17:42:30 +0100

    gpt4

--- pages/api/openai.js
+++ pages/api/openai.js
@@ -35,7 +35,7 @@ export async function handleOpenAIRequest(reqBody, reqHeaders) {
     const response = await axios.post(
       'https://api.openai.com/v1/chat/completions',
       {
-        model: 'gpt-3.5-turbo',
+        model: 'gpt-4',
         messages: messages,
         max_tokens: 2000,
       },
