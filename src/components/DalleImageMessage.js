116f37505153ee191a4d7c8594a311991e441beb
ChatBSV <ab@alanborger.com>
2023-12-06 15:21:00 +0100

    v0.92

--- src/components/DalleImageMessage.js
+++ src/components/DalleImageMessage.js
@@ -10,7 +10,7 @@ function DalleImageMessage({ content, onImageLoad, txid }) {
     <div className={`${styles.chatMessage} ${styles.imageMessage}`}>
       <p>Here's where my imagination took me:</p>
       <div className={styles.imagewrap}>
-      <img className={styles.image} src={content} alt="DALL-E Generated Image" onLoad={onImageLoad} /></div>
+      <img className={styles.image} src={content} alt="DALL-E Generated Image" onLoad={onImageLoad}/></div>
       <p>This image will be available for an hour. Right-click over the image to save it.</p>
       <div className={styles.chatLink}>
         <TxidLink txid={txid} />
