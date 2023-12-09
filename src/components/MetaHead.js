cd423f2f21c1984bb9925cd9020f7cc3e46b3ab9
ChatBSV <ab@alanborger.com>
2023-12-09 07:24:39 +0100

    <IMG> MGMT

--- src/components/MetaHead.js
+++ src/components/MetaHead.js
@@ -7,17 +7,17 @@ const MetaHead = () => (
         <title>ChatBSV - OpenAI on Bitcoin</title>
         <meta name="description" content="Ask me anything! Micro transactions at their best. Pay per use AI tokens." />
         <meta name="theme-color" content="#eee" />
-    <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/manifest.json" />
         <meta property="og:title" content="ChatBSV - AI on Bitcoin" />
         <meta property="og:description" content="Ask me anything! Micro transactions at their best. Pay per use AI tokens." />
-        <meta property="og:image" content="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/654a55ab5a25aa83e46c8d50_ChatBSV_openGraph.png" />
+        <meta property="og:image" content="/ChatBSV_openGraph.png" />
         <meta property="twitter:title" content="ChatBSV - AI on Bitcoin" />
         <meta property="twitter:description" content="Ask me anything! Micro transactions at their best. Pay per use AI tokens." />
-        <meta property="twitter:image" content="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/654a55ab5a25aa83e46c8d50_ChatBSV_openGraph.png" />
+        <meta property="twitter:image" content="/ChatBSV_openGraph.png" />
         <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <link rel="icon" href="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/654a55aba928b9384cbdbf9a_ChatBSV%20favicon.png" />
-        <link rel="apple-touch-icon" href="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/654a55ab172e4512aad4cd83_ChatBSV%20Pro_webclip.png" />
+        <meta name="twitter:card" content="/ChatBSV_openGraph.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
     </Head>
 );
 
