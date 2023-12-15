f38995771c57520d2a2a1a6a12888f341b68517f
ChatBSV <ab@alanborger.com>
2023-12-15 09:19:09 +0100

    mobile ui for help and intro

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 // src/constants.js
 
-export const CURRENT_SESSION_VERSION = '1.0.0';  // Update this value with each new build
+export const CURRENT_SESSION_VERSION = '1.0.1';  // Update this value with each new build
