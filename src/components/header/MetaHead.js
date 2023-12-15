3f913a4303343bf7505a3727180500c7b743a855
ChatBSV <ab@alanborger.com>
2023-12-15 09:25:55 +0100

    og im

--- src/components/header/MetaHead.js
+++ src/components/header/MetaHead.js
@@ -9,12 +9,12 @@ const MetaHead = () => (
         <meta name="theme-color" content="#eee" />
         <meta property="og:title" content="ChatBSV - AI on Bitcoin" />
         <meta property="og:description" content="Ask me anything! Micro transactions at their best. Pay per use AI tokens." />
-        <meta property="og:image" content="/ChatBSV_openGraph.png" />
+        <meta property="og:image" href="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/657c0d2ff552e7457d86b172_ChatBSV_openGraph.png" />
         <meta property="twitter:title" content="ChatBSV - AI on Bitcoin" />
         <meta property="twitter:description" content="Ask me anything! Micro transactions at their best. Pay per use AI tokens." />
-        <meta property="twitter:image" content="/ChatBSV_openGraph.png" />
+        <meta property="twitter:image" href="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/657c0d2ff552e7457d86b172_ChatBSV_openGraph.png" />
         <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="/ChatBSV_openGraph.png" />
+        <meta name="twitter:card" href="https://uploads-ssl.webflow.com/646064abf2ae787ad9c35019/657c0d2ff552e7457d86b172_ChatBSV_openGraph.png" />
         <link rel="icon" href="/icon-32x32.png" />
         <link rel="apple-touch-icon" href="/icon-192x192.png" />    
         </Head>
