051c0ab388767ccac3a9131be71a92a8cad3809b
ChatBSV <ab@alanborger.com>
2023-12-18 14:09:48 +0100

    threads merged

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 // src/constants.js
 
-export const CURRENT_SESSION_VERSION = '0.9.8';  // Update this value with each new build
+export const CURRENT_SESSION_VERSION = '0.9.7';  // Update this value with each new build
